35e265aa3842d43993184a176005419119160cdb
Marianela Méndez <67439634+marimenvas@users.noreply.github.com>
2020-10-10 16:59:08 -0300

    inicio configuração inicial ts pedido garcon, app.component.ts y ajuste d inicial de formulario

--- src/app/pedido-garcon/pedido-garcon.component.ts
+++ src/app/pedido-garcon/pedido-garcon.component.ts
@@ -1,14 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { PedidoService } from '../pedido.service';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-pedido-garcon',
   templateUrl: './pedido-garcon.component.html',
-  styleUrls: ['./pedido-garcon.component.css']
+  styleUrls: ['./pedido-garcon.component.css'],
+ 
 })
 export class PedidoGarconComponent implements OnInit {
 
   constructor(pedido: PedidoService) {}
+
+  pedidos = [
+    {
+      "id": 1,
+      "productId": 2,
+      "mesaid": 2,
+      "status_pedido": 1,
+    },
+
+    {
+      "id": 2,
+      "productId": 3,
+      "mesaid": 2,
+      "status_pedido": 2,
+    },
+
+    {
+      "id": 3,
+      "productId": 2,
+      "mesaid": 2,
+      "status_pedido": 1,
+    }
+  
+  ];
+
+  model:any={};
+
+  addPedido():void{
+    this.pedidos.push(this.model);
+  }
+  addGetPedido():void{
+  }
+  deletePedido():void{
+  }
+  editPedido():void{
+  }
+  updatePedido():void{
+  }
 
   ngOnInit(): void {
   }
