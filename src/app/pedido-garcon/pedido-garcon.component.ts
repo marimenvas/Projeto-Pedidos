232d4b173d7a8be928fdb24204968d44e7b368e7
Marianela Méndez <67439634+marimenvas@users.noreply.github.com>
2020-10-10 22:25:47 -0300

    função deletar

--- src/app/pedido-garcon/pedido-garcon.component.ts
+++ src/app/pedido-garcon/pedido-garcon.component.ts
@@ -43,7 +43,11 @@ export class PedidoGarconComponent implements OnInit {
   }
   addGetPedido():void{
   }
-  deletePedido():void{
+  deletePedido(i):void{
+    var answer = confirm ('Está seguro que quer deletar?');
+    if(answer){
+      this.pedidos.splice(i, 1);
+    }
   }
   editPedido():void{
   }
