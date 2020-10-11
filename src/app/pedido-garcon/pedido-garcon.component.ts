b7a09bb8a732eb380da157f0b7fdb2bb504cae27
Marianela Méndez <67439634+marimenvas@users.noreply.github.com>
2020-10-11 02:53:03 -0300

    função editar e alert success step 1

--- src/app/pedido-garcon/pedido-garcon.component.ts
+++ src/app/pedido-garcon/pedido-garcon.component.ts
@@ -12,6 +12,8 @@ export class PedidoGarconComponent implements OnInit {
 
   constructor() {}
 
+  msg:string = '';
+
   pedidos = [
     {
       "id": 1,
@@ -37,9 +39,12 @@ export class PedidoGarconComponent implements OnInit {
   ];
 
   model:any={};
+  model2:any={};
+  hideUpdate:boolean = true;
 
   addPedido():void{
     this.pedidos.push(this.model);
+    this.msg = 'pedido carregado';
   }
   addGetPedido():void{
   }
@@ -47,14 +52,33 @@ export class PedidoGarconComponent implements OnInit {
     var answer = confirm ('Está seguro que quer deletar?');
     if(answer){
       this.pedidos.splice(i, 1);
+      this.msg = 'pedido deletado';
     }
   }
-  editPedido():void{
+
+  myValue;
+  editPedido(i):void{
+    this.hideUpdate = false;
+    this.model2.mesaid = this.pedidos[i].mesaid;
+    this.model2.id = this.pedidos[i].id;
+    this.model2.productId = this.pedidos[i].productId;
+    this.myValue = i;
   }
   updatePedido():void{
+    let i = this.myValue;
+    for(let j = 0; j < this.pedidos.length;j++){
+      if(i == j) {
+        this.pedidos[i] = this.model2;
+        this.msg = 'pedido atualizado';
+        this.model2 = {};
+      }
+    }
   }
 
   ngOnInit(): void {
   }
+
+ // closeAlert(): void{
+   // this.msg = '';}
 
 }
