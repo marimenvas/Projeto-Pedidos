e3a482432260944a3121abbfc14c9573811011d4
Leonardo <leonardomarinho.nf@gmail.com>
2020-10-15 12:20:08 -0300

    Sugestões de melhoria

--- src/app/pedido-garcon/pedido-garcon.component.ts
+++ src/app/pedido-garcon/pedido-garcon.component.ts
@@ -11,10 +11,12 @@ import { enableDebugTools } from '@angular/platform-browser';
 })
 export class PedidoGarconComponent implements OnInit {
 
+  // NÃO FAZ ISSO PELO AMOR DE DEUS, url de API fica no serviço única e exclusivamente!
   readonly apiURL : string;
 
   constructor(pedidosServ: PedidoService) {
 
+    // NÃO FAZ ISSO PELO AMOR DE DEUS, url de API fica no serviço única e exclusivamente 2!
     this.apiURL = 'http://localhost:3000';
    // pedidosServ.getPedidos().subscribe(pedidos=> {
      // this.pedidos = pedidos
@@ -28,10 +30,12 @@ export class PedidoGarconComponent implements OnInit {
 
   msg:string = ''
 
+  // Aqui siiim :D, lá no service NÃO! Esses arrays de pedidos e pedido precisam ficar aqui!
   pedidos: any = [];
   pedido: any = [];
  
-
+  // Model? Chamando o próprio componente? Que? 
+  // Esse código não fez sentido para mim, está funcionando corretamente?
   model:any={PedidoGarconComponent};
   model2:any={};
   hideUpdate:boolean = true;
@@ -41,8 +45,13 @@ export class PedidoGarconComponent implements OnInit {
     this.pedidos.push(this.model);
     this.msg = 'pedido carregado';
   }
+
+  // Se não usa, tira :). Mantenha o código limpo
   addGetPedido():void{
   }
+
+  // ele só deleta da lista na tela, não deveria enviar para a api também?
+  // E quando a api retornar sucesso na remoção, atualizar a lista para o usuário que está vendo
   deletePedido(i):void{
     var answer = confirm ('Está seguro que quer deletar?');
     if(answer){
@@ -51,7 +60,14 @@ export class PedidoGarconComponent implements OnInit {
     }
   }
 
+  // Variável declarada no meio do código? Não faça isso!
+  // declare tudo ná em cima no começo para manter a organização
   myValue;
+
+  // é interessante informar o tipo do método, mas, não é obrigatório!
+  // então se quiser, pode remover o :void para simplificar
+  // Não use "i" como nome de variável, use nomes mais descritivos. 
+  // Olhando "i" não sei do que se tratam esses dados
   editPedido(i):void{
     this.hideUpdate = false;
     this.model2.mesaid = this.pedidos[i].mesaid;
@@ -60,6 +76,7 @@ export class PedidoGarconComponent implements OnInit {
     this.myValue = i;
   }
 
+  // Não faria mais sentido esse método receber os dados que precisa atualizar no pedido?
   updatePedido():void{
     let i = this.myValue;
     for(let j = 0; j < this.pedidos.length;j++){
@@ -71,6 +88,8 @@ export class PedidoGarconComponent implements OnInit {
     }
   }
 
+  // Se não tem utidade, não faz sentido manter. Pode tirar!
+  // tira também o "implements OnInit" lá no início da classe
   ngOnInit(): void {
   }
 
--- src/app/pedido.service.ts
+++ src/app/pedido.service.ts
@@ -3,9 +3,9 @@ import { PedidoGarconComponent } from "./pedido-garcon/pedido-garcon.component";
 import { PedidosCocinaComponent } from "./pedidos-cocina/pedidos-cocina.component";
 
 import { HttpClient } from '@angular/common/http';
+
+// Tira essa linha, não precisa
 import { Observable } from 'rxjs'
-
-
 
 
 @Injectable({
@@ -15,23 +15,35 @@ export class PedidoService {
 
   private endPoint = 'http://localhost:3000/pedidos'
 
+  // Não faça isso!
   formData: PedidoGarconComponent
+  // Nem isso :P, serviço não se comunica com outros componentes, os 
+  // outros componentes que se comunicam com o serviço 
   formDataVacio: PedidoGarconComponent [];
 
+  // Serviço não fica com os dados, tira! 
+  // Um serviço apenas recebe dados e repassa ao componente, não deve armazenar ou gerenciar
   pedidos: any = [];
   pedido: any = [];
   
   constructor(private http:HttpClient) {}
 
+  // Tira o :Observable<any>, não precisa também
   public todos():Observable<any> {
+
+    // Não precisa usar o `${}`, isso serve para unir strings diferentes
+    // Aqui você não une nenhuma sting, apenas coloca a de URL
+    // Use assim:  this.http.get(this.endPoint});'
     return this.http.get(`${this.endPoint}`);
   }
 
+  // Para que serve essa função?? Tira! Não precisamos dela
   private extractData(res: Response){
     return res.json() || {};
 
   }
 
+  // Nenhum lugar do projeto usa essa função, tira!
   UpdatePedido(){
     var body= {
       ...this.formData,
@@ -40,29 +52,50 @@ export class PedidoService {
     //return this.http.post(environment.apiURL+'/pedido', body);
   }
 
-
+    // O todos faz o que esse método faz, não faz sentido usar ele, tira!
     getTodos() {
        return this.pedidos;
     }
 
     getPedidos(){
+      // Não coloque a string toda, se você quiser mudar a URL do servidor, você precisa mudar em cada
+      // função. Use o da variável endPoint
       return this.http.get('http://localhost:3000/pedidos')
     }
 
+    // Cadê o id? Como a API vai saber qual pedido deve excluir?
+    // Passe para esta função um ID: deletePedidos()
     deletePedidos(){
+      // Não coloque a string toda, se você quiser mudar a URL do servidor, você precisa mudar em cada
+      // função. Use o da variável endPoint e concatene com o id. Aqui você pode usar o `${}`
+      // Exemplo: `${this.endPoint}/${id}`
       return this.http.delete('http://localhost:3000/pedido/:id')
     }
 
     postPedido(formData) {
+       // Não coloque a string toda, se você quiser mudar a URL do servidor, você precisa mudar em cada
+      // função. Use o da variável endPoint
         return this.http.post('http://localhost:3000/pedido',formData);
       }
 
+
+    // Cadê os dados do pedido? Como o método vai saber o que enviar para a api???
+    // Este método tem que ser algo como: putPedido (id, data)
     putPedido () {
+         // Não coloque a string toda, se você quiser mudar a URL do servidor, você precisa mudar em cada
+        // função. Use o da variável endPoint
+        // o formData tem que vir como parâmetro do método, não de uma variável global do serviço. 
+        // NÃO USE variável global em serviço. Serviço não é componente!!!
         return this.http.put('http://localhost:3000/pedido/:id',this.formData);
       }
     
    // return this.http.get('http://localhost:3000/pedidos/:id')
     
+
+
+// JAMAIS use subscribe aqui. Subscribe tem que ser no componente que irá consumir os dados, ok? 
+
+
       //this.http.post('http://localhost:3000/pedido', pedido)
                 //.subscribe(
                  // resultado => {
@@ -71,11 +104,10 @@ export class PedidoService {
                  // erro => {
                     //if(erro.status == 400) {
                      // console.log(erro);
-                    }
+                    // }
                  // }
                 //);
     //}
     
   
-//}
- 
+}
