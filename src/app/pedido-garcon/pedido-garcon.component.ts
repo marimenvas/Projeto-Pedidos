631fb94c899cb5ece34377f5f5d26ae988a06513
Marianela Méndez <67439634+marimenvas@users.noreply.github.com>
2020-10-19 20:42:57 -0300

    adicionei metodo get e post. Está funcionando

--- src/app/pedido-garcon/pedido-garcon.component.ts
+++ src/app/pedido-garcon/pedido-garcon.component.ts
@@ -9,40 +9,44 @@ import { enableDebugTools } from '@angular/platform-browser';
   styleUrls: ['./pedido-garcon.component.css'],
  
 })
-export class PedidoGarconComponent implements OnInit {
 
-  readonly apiURL : string;
+export class PedidoGarconComponent {//implements OnInit {  
 
-  constructor(pedidosServ: PedidoService) {
+  msg:string = '';
+  
+  myValue;
 
-    this.apiURL = 'http://localhost:3000';
-   // pedidosServ.getPedidos().subscribe(pedidos=> {
-     // this.pedidos = pedidos
-    //});
-
-   // pedidosServ.postPedido().subscribe(pedido => {
-    //this.pedido = pedido
-    //});
-    
-  }
-
-  msg:string = ''
-
-  pedidos: any = [];
   pedido: any = [];
- 
+  pedidos: any = [];
 
-  model:any={PedidoGarconComponent};
+  model:any={};
   model2:any={};
   hideUpdate:boolean = true;
 
+  onSubmit(formulario){
+    this.pedidoServ.postPedidos(formulario.form.value)
+    .subscribe(resposta => {
+      console.log(resposta);
+      alert('Pedido enviado com sucesso!');
+
+    });
+    //console.log(formulario.form.value)
+
+  }
+
+  constructor(private pedidoServ: PedidoService) {
+    //this.pedidos = pedidosServ.getTodos();// leva a get todos
+
+    pedidoServ.getPedidos().subscribe(pedidos=> {
+      this.pedidos = pedidos
+    });
+  }
+
   addPedido():void{
-  
     this.pedidos.push(this.model);
     this.msg = 'pedido carregado';
   }
-  addGetPedido():void{
-  }
+
   deletePedido(i):void{
     var answer = confirm ('Está seguro que quer deletar?');
     if(answer){
@@ -51,8 +55,7 @@ export class PedidoGarconComponent implements OnInit {
     }
   }
 
-  myValue;
-  editPedido(i):void{
+  editPedido(i):void{// para fazer mudanças no angular não faz enviou para a API
     this.hideUpdate = false;
     this.model2.mesaid = this.pedidos[i].mesaid;
     this.model2.id = this.pedidos[i].id;
@@ -60,7 +63,7 @@ export class PedidoGarconComponent implements OnInit {
     this.myValue = i;
   }
 
-  updatePedido():void{
+  updatePedido():void{ // para fazer mudanças no angular não faz enviou para a API
     let i = this.myValue;
     for(let j = 0; j < this.pedidos.length;j++){
       if(i == j) {
@@ -71,7 +74,7 @@ export class PedidoGarconComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {// da erro si tiro
   }
 
   closeAlert(): void{
--- src/app/pedido.service.ts
+++ src/app/pedido.service.ts
@@ -3,79 +3,38 @@ import { PedidoGarconComponent } from "./pedido-garcon/pedido-garcon.component";
 import { PedidosCocinaComponent } from "./pedidos-cocina/pedidos-cocina.component";
 
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs'
-
-
 
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PedidoService {
-
+  
   private endPoint = 'http://localhost:3000/pedidos'
+  
 
-  formData: PedidoGarconComponent
-  formDataVacio: PedidoGarconComponent [];
-
-  pedidos: any = [];
-  pedido: any = [];
+  //pedidos: any = [];//quando tiro da error, mas tenho que tirar
+  //pedido: any = [];
   
   constructor(private http:HttpClient) {}
 
-  public todos():Observable<any> {
-    return this.http.get(`${this.endPoint}`);
-  }
 
-  private extractData(res: Response){
-    return res.json() || {};
-
-  }
-
-  UpdatePedido(){
-    var body= {
-      ...this.formData,
-      formDataVacio: this.formDataVacio
-    };
-    //return this.http.post(environment.apiURL+'/pedido', body);
-  }
-
-
-    getTodos() {
-       return this.pedidos;
-    }
+   // getTodos() {
+     //  return this.pedidos;// precisa de pedidos
+   // }
 
     getPedidos(){
-      return this.http.get('http://localhost:3000/pedidos')
+      return this.http.get(this.endPoint)
     }
 
-    deletePedidos(){
-      return this.http.delete('http://localhost:3000/pedido/:id')
+    postPedidos(formData) {
+      return this.http.post('http://localhost:3000/pedido',formData) 
     }
 
-    postPedido(formData) {
-        return this.http.post('http://localhost:3000/pedido',formData);
-      }
+    deletePedidos(id){
+      return this.http.delete(`${this.endPoint}/${id}`)
+    }
 
-    putPedido () {
-        return this.http.put('http://localhost:3000/pedido/:id',this.formData);
-      }
-    
-   // return this.http.get('http://localhost:3000/pedidos/:id')
-    
-      //this.http.post('http://localhost:3000/pedido', pedido)
-                //.subscribe(
-                 // resultado => {
-                   // console.log(resultado)
-                  //},
-                 // erro => {
-                    //if(erro.status == 400) {
-                     // console.log(erro);
-                    }
-                 // }
-                //);
-    //}
-    
-  
-//}
- 
+}
+                
